4a81ca4d1fbd9c33a91f0975416958f7a60a6f33
Nikita Shatov <xamenrax@gmail.com>
2020-03-20 18:23:24 +0300

    Add PLAINTEXT signature

--- lib/oauth.rb
+++ lib/oauth.rb
@@ -6,6 +6,7 @@ require 'oauth/version'
 require 'oauth/oauth'
 
 require 'oauth/client/helper'
+require 'oauth/signature/plaintext'
 require 'oauth/signature/hmac/sha1'
 require 'oauth/signature/rsa/sha1'
 require 'oauth/request_proxy/mock_request'
